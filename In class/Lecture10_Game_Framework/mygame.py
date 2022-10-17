006f1d47b940eebb0c13f59b803e1b2dc6267f30
onehana1 <73771420+onehana1@users.noreply.github.com>
2022-10-17 13:25:08 +0900

    drill 10
    2019182007 김문수

--- In class/Lecture10_Game_Framework/mygame.py
+++ In class/Lecture10_Game_Framework/mygame.py
@@ -2,8 +2,11 @@ import pico2d
 import game_framework
 
 import logo_state
+import play_state
+import item_state
+import boy_add_delete
 
 pico2d.open_canvas()
-game_framework.run(logo_state)
+game_framework.run(play_state)
 pico2d.close_canvas()
 # fill here
